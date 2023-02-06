09aff62cfbed924219b97d9115af2617c9742cbf
EshuShango <tobiakinyemi21@gmail.com>
2023-02-06 17:59:22 -0600

    upd file, delete

--- Develop/public/assets/js/index.js
+++ Develop/public/assets/js/index.js
@@ -1,3 +1,5 @@
+// console.log("connection test");
+
 let noteTitle;
 let noteText;
 let saveNoteBtn;
--- Develop/server.js
+++ Develop/server.js
@@ -1,18 +1,20 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 //* tells EXPRESS this folder is okay to use
 app.use(express.static("public"));
 
-//& func for route
+//& function to create unique id by time stamping 
 const getData = () => {
-  const db = fs.readFileSync(path.join(__dirname, "db", "db.json"), "utf-8");
+  // const db = fs.readFileSync(path.join(__dirname, "db", "db.json","utf-8"));
+  const db = fs.readFileSync(("./db/db.json"));
   const parsedJson = JSON.parse(db);
   return parsedJson;
 };
@@ -24,90 +26,68 @@ app.get("/api/notes", (req, res) => {
   res.json(db);
 });
 app.post("/api/notes", (req, res) => {
-  console.log(req.body);
+  // console.log(req.body);
   let noteObj = {
     title: req.body.title,
     text: req.body.text,
-    id: Date.now()
-  }
+    id: Date.now(),
+  };
   // take data thats going into the request body
-  // make into a object 
-  //add that to noteId 
+  // make into a object
+  //add that to noteId
   let db = getData();
+  // console.log(db);
   db.push(noteObj);
-  const fileName =  path.join(__dirname, "db", "db.json")
-  fs.writeFileSync(fileName, JSON.stringify(db, null, 2) );
+  const fileName = path.join(__dirname, "./db/db.json");
+  fs.writeFileSync(fileName, JSON.stringify(db, null, 2));
   res.json(db);
 });
 
+
+//!------------------Delete ----
+
+// app.delete("/api/notes/:id", (req, res) => {
+//   let noteId = req.params.id;
+//   let grabData = getData();
+//   console.log(grabData)
+//   let deleteNote = grabData.find(note => note.id === noteId );
+//   console.log(deleteNote);
+//   let noteIndex = grabData.indexOf(deleteNote);
+//   // console.log(noteIndex);
+//   grabData.splice(noteIndex, 1);
+//   fs.writeFileSync( "./db/db.json", JSON.stringify(grabData));
+//   res.json(getData());
+// });
+
+app.delete("/api/notes/:id", async (req, res) => {
+  let noteId = req.params.id;
+  let grabData = await getData();
+  let deleteNote = grabData.find(note => note.id === noteId );
+  console.log(deleteNote)
+  let noteIndex = grabData.indexOf(deleteNote);
+  console.log(noteIndex)
+  grabData.splice(noteIndex, 1);
+  const data = await fs.promises.writeFile('./db/db.json', JSON.stringify(grabData));
+  res.json(getData());
+});
+
+//!---------------
+
 //^-------------
 
-// '^/$|/index.(html)?
 app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "notes.html"));
+  // res.sendFile(path.join(__dirname, "public/notes.html"));
+  // res.send("test test")
 });
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+  // res.sendFile(path.join(__dirname, "public", "index.html"));
+  res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
-//!--------
-// app.get('/', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'index.html'));
-//   });
-//!--------
-
-//^-----ADDED CODE
-// //Get route for notes page
-// app.get("/notes", (req, res) => {
-//   res.sendFile(path.join(__dirname, "public/notes.html"));
-//   console.info(`${req.method} request received for\n/Notes`);
-// });
-
-// //Get route for home page
-// app.get("/index", (req, res) => {
-//   res.sendFile(path.join(__dirname, "public/index.html"));
-//   console.info(`${req.method} request received for \n/Index`);
-// });
-
-//^----------
-
-//!----------IDK the code below------
 app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`);
+  
 });
-//!--------
 
-//^-----------INFO IDK what todo with yet----
-
-// GET request
-//
-// app.get('/api/reviews', (req, res) => {
-//   // Let the client know that their request was received
-//   res.json(`${req.method} request received`);
-
-//   // Show the user agent information in the terminal
-//   console.info(req.rawHeaders);
-
-//   // Log our request to the terminal
-//   console.info(`${req.method} request received`);
-// });
-//^----------
-
-//^-----------INFO IDK what todo with yet----
-// POST request
-// app.post('/api/reviews', (req, res) => {
-//   // Let the client know that their POST request was received
-//   res.json(`${req.method} request received`);
-
-//   // Show the user agent information in the terminal
-//   console.info(req.rawHeaders);
-
-//   // Log our request to the terminal
-//   console.info(`${req.method} request received`);
-// });
-//^----------
-
-// app.listen(PORT, () =>
-// console.log(`Express server listening on port ${PORT}!`)
-// );
